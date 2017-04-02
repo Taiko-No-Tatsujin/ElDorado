0943d1bfcb5f53ebb82060799aaa2fa0ba7da249
mitagi <itagi89@gmail.com>
2017-04-02 20:39:15 +0530

    Login Page

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,4 @@
+/// <reference path="globals/material-ui/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/react-router/index.d.ts" />
+/// <reference path="globals/validator/index.d.ts" />
